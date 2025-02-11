b054da5294f633883b86b494f3c4893fcaf0ccb6
eqbal <eqbal.sharaf20@gmail.com>
2025-02-11 17:52:00 +0430

    Changed the title

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="text-red-500 border text-center">This is coming from the Home component</h1>
+            <h1 className="text-red-500 border text-center">Hey! Welcome to Job Seek, Where you can find your desired job.</h1>
         </>
     )
 }
