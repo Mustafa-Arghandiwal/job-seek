8d2be53994a8aa62c289d3766cb22fe9a1fa2715
Mustafa ARGHANDIWAL <mustafa.arghandiwal@reach-initiative.org>
2025-03-01 23:03:46 +0430

    custom font

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="text-green-400 border text-center">Hey! Welcome to Job Seek, Where you can find your desired job.</h1>
+            <h1 className="text-slate-950 font-inter border text-center">Hey! Welcome to Job Seek, Where you can find your desired job.</h1>
         </>
     )
 }
