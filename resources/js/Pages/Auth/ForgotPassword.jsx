5ac2703167b9bdb306fa8666feef303fe968785b
Eqbal-Sharaf <eqbal.sharaf20@gmail.com>
2025-03-09 17:49:24 +0430

    Added some responsiveness

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -28,18 +28,18 @@ export default function ForgotPassword() {
     }
 
     return (
-        <div className="font-inter flex h-[100svh]  justify-between relative">
+        <div className="font-inter h-[100svh] grid grid-cols-1 md:grid-cols-2 md:grid-rows-1">
             
-            <div className="grid place-items-center w-[50vw] relative">
+            <div className="flex flex-col relative">
 
-                <div className="absolute top-2 left-8 flex items-center gap-1">
+                <div className="flex items-center gap-1 px-4 py-2">
                     <img src="briefcase.svg" />
                     <span className="font-semibold text-2xl">JobSeek</span>
                 </div>
 
-                <div className="p-2 min-w-[536px] flex flex-col gap-7">
+                <div className="flex flex-col justify-center items-center h-screen gap-18 px-4 py-2">
 
-                    <div className="flex justify-between items-center">
+                    
                         <div className="flex flex-col gap-2">
                             <h1 className="text-gray-900 text-[32px]">Forgot Your Password?</h1>
                             <div className="flex gap-1">
@@ -50,33 +50,31 @@ export default function ForgotPassword() {
                                 <p className="text-gray-600">Don't have an account?</p>
                                 <Link className="text-[#0A65CC]" href="/sign-up">Create Account</Link>
                             </div>
-
                         </div>
 
+                    {/* <div className=""> */}
+                        <form onSubmit={handleSubmit} className=" max-w-[520px] flex flex-col flex-1 w-96">
+                            <div className="h-16">
+                                <input type="text" placeholder="Email Address" value={data.email} onChange={(e) => setData('email', e.target.value)} className="h-12 w-full rounded-md border border-gray-100 px-3 outline-none" />
+                                    {errors.email && <p className="text-sm text-danger-600">{errors.email}</p>}
+                            </div>
+
+                            <button disabled={processing} className="w-full mt-3 border rounded-sm flex justify-center items-center gap-3 h-14 bg-primary-500 hover:bg-primary-600 disabled:bg-[#CEE0F5] text-white font-semibold cursor-pointer select-none">
+                            Send Reset Link <img src="arrow-right.svg" />
+                            </button>
+
+                            <p className={`font-medium text-center text-success-600 opacity-0 transition-opacity h-10 mt-1 duration-500 ease-in-out ${flashVisible ? "opacity-100": "opacity-0"} `} >
+                            {flash.status}
+                            </p>
+                        </form>
+                    {/* </div> */}
                         
 
-                    </div>
-
-                    <form onSubmit={handleSubmit}>
-                        <div className="h-16">
-                                    <input type="text" placeholder="Email Address" value={data.email} onChange={(e) => setData('email', e.target.value)} className="h-12 w-full rounded-md border border-gray-100 px-3 outline-none" />
-                                    {errors.email && <p className="text-sm text-danger-600">{errors.email}</p>}
-                        </div>
-
-                        <button disabled={processing} className="w-full mt-3 border rounded-sm flex justify-center items-center gap-3 h-14 bg-primary-500 hover:bg-primary-600 disabled:bg-[#CEE0F5] text-white font-semibold cursor-pointer select-none">
-                            Send Reset Link <img src="arrow-right.svg" />
-                        </button>
-
-                        <p className={`font-medium text-center text-success-600 opacity-0 transition-opacity h-10 mt-1 duration-500 ease-in-out ${flashVisible ? "opacity-100": "opacity-0"} `} >
-                            {flash.status}
-                        </p>
-                    </form>
-
+                    
                 </div>
-
             </div>
 
-            <div className="relative bg-[url('bg2.jpg')]  w-[50vw] bg-cover bg-center">
+            <div className="relative bg-[url('bg2.jpg')]  w-[50vw] bg-cover bg-center invisible md:visible">
                 <div className="flex flex-col gap-10 max-w-[500px]  absolute bottom-10 left-22">
                     <h2 className="text-[40px] font-medium text-white">
                         Over 300 companies waiting for good employees
--- resources/js/Pages/Auth/SignIn.jsx
+++ resources/js/Pages/Auth/SignIn.jsx
@@ -16,31 +16,24 @@ export default function SignIn() {
     }
 
     return (
-        <div className="font-inter flex h-[100svh]  justify-between relative">
-            <div className="grid place-items-center w-[50vw] relative">
+        <div className="font-inter grid grid-cols-1 md:grid-rows-1 md:grid-cols-2 h-[100svh] relative">
+            <div className="flex flex-col relative">
 
-                <div className="absolute top-2 left-8 flex items-center gap-1">
+                <div className="flex gap-1 px-4 py-2 mb-4">
                     <img src="briefcase.svg" />
                     <span className="font-semibold text-2xl">JobSeek</span>
                 </div>
 
-                <div className="p-2 min-w-[536px] flex flex-col gap-7">
+                <div className="flex flex-col items-center gap-7 px-4 py-2">
 
-                    <div className="flex justify-between items-center">
-                        <div className="flex flex-col gap-2">
+                        <div className="">
                             <h1 className="text-gray-900 text-[32px]">Sign in</h1>
-                            <div className="flex gap-1">
-                                <p className="text-gray-600">Don't have an account?</p>
-                                <Link className="text-[#0A65CC]" href="/sign-up">Create Account</Link>
-                            </div>
-
+                            <p className="text-gray-600">Don't have an account?
+                                <a className="text-[#0A65CC]" href="/sign-up"> Create Account</a>
+                            </p>
                         </div>
 
-                        
-
-                    </div>
-
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4 flex-1 w-full max-w-[520px]">
                         
                         <div className="h-16">
                             <input type="text" placeholder="Email Address" value={data.email} onChange={(e) => setData('email', e.target.value)} className="h-12 w-full rounded-md border border-gray-100 px-3 outline-none" />
@@ -56,20 +49,16 @@ export default function SignIn() {
                                 </button>
                             </div>
                             {errors.password && <p className="text-sm text-danger-600">{errors.password}</p>}
-
                         </div>
-                                
-                        
 
-                        
-                            <div className="flex justify-between gap-0.5 ">
-                                <label className="text-sm text-gray-600 flex gap-2 cursor-pointer">
-                                    <input type="checkbox" checked={data.remember_me} onChange={(e) => setData('remember_me', e.target.checked)} className={`w-5 h-5 cursor-pointer transition-all ${errors.agree_terms && !data.agree_terms ? "ring-2 ring-offset-2 ring-danger-600" : ""}`}/>
+                        <div className="flex justify-between gap-0.5 ">
+                            <label className="text-sm text-gray-600 flex gap-2 cursor-pointer">
+                                <input type="checkbox" checked={data.remember_me} onChange={(e) => setData('remember_me', e.target.checked)} className={`w-5 h-5 cursor-pointer transition-all ${errors.agree_terms && !data.agree_terms ? "ring-2 ring-offset-2 ring-danger-600" : ""}`}/>
                                     Remember Me
-                                </label>
+                            </label>
 
-                                <Link className="text-primary-500 text-sm font-medium" href="/forgot-password">Forgot password?</Link>
-                            </div>
+                            <Link className="text-primary-500 text-sm font-medium" href="/forgot-password">Forgot password?</Link>
+                        </div>
                         
                         <div className="h-20">
                             <div className="h-5">
@@ -80,13 +69,11 @@ export default function SignIn() {
                                 Sign In <img src="arrow-right.svg" />
                             </button>
                         </div>
-
                     </form>
                 </div>
-
             </div>
 
-            <div className="relative bg-[url('bg2.jpg')]  w-[50vw] bg-cover bg-center">
+            <div className="relative bg-[url('bg2.jpg')]  bg-cover bg-center invisible md:visible md:w-[50vw]">
                 <div className="flex flex-col gap-10 max-w-[500px]  absolute bottom-10 left-22">
                     <h2 className="text-[40px] font-medium text-white">
                         Over 300 companies waiting for good employees
