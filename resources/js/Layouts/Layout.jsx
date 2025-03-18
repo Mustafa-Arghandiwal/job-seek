33aa14b2b8e4d139da0cd761e4625ea78e9bd312
Eqbal-Sharaf <eqbal.sharaf20@gmail.com>
2025-03-18 20:55:28 +0430

    responsive footer added

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -142,6 +142,104 @@ export default function Layout({ children }) {
                 <div className={`fixed inset-0 bg-[#18191C]/60 ${dropdownVisible ? 'opacity-100 z-40' : 'opacity-0 -z-50'}`}></div>
                 {children}
             </main>
+
+            <footer>
+                <div className="flex flex-col gap-8 bg-gray-900">
+                    <div className="grid grid-cols-2 grid-rows-auto gap-y-8 gap-x-2 py-8 px-4 lg:grid-rows-1 lg:grid-cols-6 lg:gap-x-4 lg:px-12 max-w-[1320px] mx-auto">
+                        <div className="flex flex-col gap-4 col-span-2">
+                            <div className="flex items-center gap-1">
+                                <img src="briefcase.svg"/>
+                                <span className="font-semibold text-2xl text-white">JobSeek</span>
+                            </div>
+                            <div className="flex flex-col gap-3">
+                                <p className="text-gray-400">
+                                    Call now: <span className="text-white">+93-777-777-777</span>
+                                </p>
+                                <p className="text-gray-400">
+                                    6391 Elgin St. Celina, Delaware 10299, New York, United Stated of America
+                                </p>
+                            </div>
+
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            <h3 className="text-white font-semibold text-xl">Quick Links</h3>
+                            <ul className="flex flex-col gap-3">
+                                <li className="text-gray-400">
+                                    <Link href="" className="">About</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Contact</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Pricing</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Blog</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            <h3 className="text-white font-semibold text-xl">Candidate</h3>
+                            <ul className="flex flex-col gap-3">
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Browse Jobs</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Browse Employers</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Candidate Dashboard</Link>
+                                </li>
+                               <li className="text-gray-400">
+                                <Link href="" className="">Saved Jobs</Link>
+                               </li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            <h3 className="text-white font-semibold text-xl">Employers</h3>
+                            <ul className="flex flex-col gap-3">
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Post a Job</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Browse Candidates</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Employers Dashboard</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Applications</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            <h3 className="text-white font-semibold text-xl">Support</h3>
+                            <ul className="flex flex-col gap-3">
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Faqs</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Privacy Policy</Link>
+                                </li>
+                                <li className="text-gray-400">
+                                    <Link href="" className="">Terms & Conditions</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="border-t border-gray-600 ">
+                        <div className="flex justify-center items-center lg:justify-between max-w-[1320px] px-4 py-4 lg:px-12 mx-auto">
+                            <p className="text-gray-400">@ 2024 MyJob - Job Portal. All rights reserved.</p>
+                            <div className=" hidden lg:flex lg:gap-1">
+                                <img src="briefcase.svg" className="" />
+                                <img src="briefcase.svg" className="" />
+                                <img src="briefcase.svg" className="" />
+                                <img src="briefcase.svg" className="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
