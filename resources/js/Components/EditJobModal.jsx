14ea60954168c73453ba8539ab00880327069a3f
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-07-06 17:11:14 +0430

    Add Edit Job Modal

---
+++ resources/js/Components/EditJobModal.jsx
@@ -0,0 +1,294 @@
+import { useForm, usePage } from "@inertiajs/react";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import Select from "./Select";
+import RichTextEditor from "./RichTextEditor";
+import DatePicker from "./DatePicker";
+
+
+
+export default function EditJobModal({ close, showModal }) {
+
+    const { props } = usePage({})
+    const { data, setData, reset, errors, processing, post } = useForm({
+        jobTitle: '',
+        salaryType: '',
+        salaryFormat: '',
+        fixedSalary: '',
+        minSalary: '',
+        maxSalary: '',
+        education: '',
+        experience: '',
+        jobLevel: '',
+        jobType: '',
+        workMode: '',
+        city: '',
+        deadline: '',
+        description: '',
+        responsibilities: '',
+
+    })
+
+
+    const handleSelectChange = (field, option) => {
+        setData(prev => ({
+            ...prev,
+            [field]: option,
+        }))
+    }
+    const handleDeadlineChange = (value) => {
+        setData(prev => ({
+            ...prev,
+            deadline: value
+        }))
+    }
+
+
+    const [successMsg, setSuccessMsg] = useState('')
+    useEffect(() => {
+        if (props.flash.postJobSuccess) {
+            setSuccessMsg(props.flash.postJobSuccess)
+        }
+    }, [props.flash.postJobSuccess])
+
+    useEffect(() => {
+        if (successMsg) {
+            const timer = setTimeout(() => setSuccessMsg(''), 5000)
+            return () => clearTimeout(timer)
+        }
+    }, [successMsg])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        post('/employer/vacancies', {
+            onSuccess: () => {
+                reset()
+            }
+        })
+
+    }
+
+    const root = document.getElementById("react-portal-root")
+    return createPortal(
+        <div className={`inset-0 bg-black/60 z-50  fixed flex justify-center items-center transition-opacity duration-200 ${showModal ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
+            <div className="relative w-[80dvw] h-[80dvh] bg-white   rounded-xl p-7 sm:p-12 overflow-y-auto scrollbar-custom">
+                <div className="sticky  z-10 flex justify-end -mt-6 -mr-6 sm:-mr-10 -top-6 sm:-top-10">
+                    <button onClick={close} className="bg-white  w-7 sm:w-10 h-7 sm:h-10 rounded-full border border-customGray-200 grid place-items-center cursor-pointer active:scale-95">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.625 4.375L4.375 15.625" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15.625 15.625L4.375 4.375" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+
+
+
+
+                {/* form here */}
+                <form onSubmit={handleSubmit}>
+
+                    <h1 className="font-medium text-2xl text-customGray-900 ">Edit Job [job name]</h1>
+
+                    <div className="mt-8">
+
+                        <div className="flex  flex-col w-full  min-w-44 ">
+                            <label htmlFor="jobTitle" className="text-sm text-customGray-900">Job Title</label>
+                            <input type="text" placeholder="e.g. Finance Officer" id="jobTitle" value={data.jobTitle} onChange={(e) => setData('jobTitle', e.target.value)} className="mt-2 rounded-md border border-customGray-100 placeholder:text-customGray-400 text-customGray-900 outline-none focus:ring-1 focus:ring-primary-500 py-[11px] px-[18px]" />
+                            <div className="text-sm w-full text-danger-600 min-h-5" >
+                                {errors.jobTitle}
+                            </div>
+                        </div>
+
+
+                        <div className="mt-3">
+
+                            <h2 className="text-lg font-medium text-customGray-900">Salary</h2>
+
+                            <div>
+                                <div className="flex gap-3 lg:gap-5  lg:items-center  flex-col lg:flex-row ">
+
+                                    <div className=" mt-3 w-full max-w-64  min-w-32  ">
+                                        <label className="text-sm text-customGray-900">Salary Type</label>
+                                        <Select options={['Hourly', 'Daily', 'Weekly', 'Monthly', 'Commission-based', 'Negotiable']} placeholder={data.salaryType} onValueChange={(option) => handleSelectChange('salaryType', option)} />
+                                        <div className="text-sm w-full text-danger-600 min-h-5 " >
+                                            {errors.salaryType || ''}
+                                        </div>
+                                    </div>
+
+                                    {
+                                        !['', 'Commission-based', 'Negotiable'].includes(data.salaryType) &&
+
+                                        <div className=" lg:mt-6 relative">
+                                            <div className="w-full  max-w-fit  flex flex-col xs:flex-row gap-2 xs:gap-4  text-customGray-900 text-sm ">
+                                                <label className="cursor-pointer flex  gap-1">
+                                                    <input type="radio" name="salaryFormat" value="Fixed Amount" checked={data.salaryFormat === 'Fixed Amount'} onChange={e => setData('salaryFormat', e.target.value)} className="cursor-pointer" />
+                                                    <span>Fixed Amount</span>
+                                                </label>
+                                                <label className="cursor-pointer flex  gap-1" >
+                                                    <input type="radio" name="salaryFormat" value="Salary Range" checked={data.salaryFormat === 'Salary Range'} onChange={e => setData('salaryFormat', e.target.value)} className="cursor-pointer" />
+                                                    <span>Salary Range</span>
+                                                </label>
+
+                                            </div>
+
+                                            <div className="text-sm w-full text-danger-600 min-h-5 lg:absolute lg:-bottom-[35px]" >
+                                                {errors.salaryFormat || ''}
+                                            </div>
+                                        </div>
+
+                                    }
+                                </div>
+
+
+                                {
+                                    (!['', 'Commission-based', 'Negotiable'].includes(data.salaryType) && data.salaryFormat === 'Fixed Amount') &&
+
+                                    <div className=" flex flex-col w-full min-w-32 max-w-64 mt-4">
+                                        <label htmlFor="salary" className="text-sm text-customGray-900">Salary</label>
+                                        <input type="number" placeholder="Salary..." id="salary" value={data.fixedSalary} onChange={(e) => setData('fixedSalary', e.target.value)} className="mt-2 rounded-md border border-customGray-100 placeholder:text-customGray-400 text-customGray-900 outline-none focus:ring-1 focus:ring-primary-500 py-[11px] px-[18px]" />
+                                        <div className="text-sm w-full text-danger-600 min-h-5" >
+                                            {errors.fixedSalary || ''}
+                                        </div>
+                                    </div>
+                                }
+
+                                {
+                                    (!['', 'Commission-based', 'Negotiable'].includes(data.salaryType) && data.salaryFormat === 'Salary Range') &&
+
+                                    <div className="flex gap-2 xs:gap-5 flex-wrap xs:flex-nowrap mt-4">
+                                        <div className="flex flex-col w-full min-w-32 max-w-64 ">
+                                            <label htmlFor="minSalary" className="text-sm text-customGray-900">Min Salary</label>
+                                            <input type="number" placeholder="Min salary..." id="minSalary" value={data.minSalary} onChange={(e) => setData('minSalary', e.target.value)} className="mt-2 rounded-md border border-customGray-100 placeholder:text-customGray-400 text-customGray-900 outline-none focus:ring-1 focus:ring-primary-500 py-[11px] px-[18px]" />
+                                            <div className="text-sm w-full text-danger-600 min-h-5" >
+                                                {errors.minSalary || ''}
+                                            </div>
+                                        </div>
+
+                                        <div className="flex flex-col w-full min-w-32 max-w-64 ">
+                                            <label htmlFor="maxSalary" className="text-sm text-customGray-900">Max Salary</label>
+                                            <input type="number" placeholder="Max salary..." id="maxSalary" value={data.maxSalary} onChange={(e) => setData('maxSalary', e.target.value)} className="mt-2 rounded-md border border-customGray-100 placeholder:text-customGray-400 text-customGray-900 outline-none focus:ring-1 focus:ring-primary-500 py-[11px] px-[18px]" />
+                                            <div className="text-sm w-full text-danger-600 min-h-5" >
+                                                {errors.maxSalary || ''}
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+
+
+                            </div>
+                        </div>
+
+
+                        <h2 className="text-lg font-medium text-customGray-900 mt-3">Additional Details</h2>
+
+                        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-2 mt-3">
+
+                            <div className="w-full max-w-64  min-w-32">
+                                <label className="text-sm text-customGray-900">Education</label>
+                                <Select options={["No formal education", "High School Diploma", "Associate Degree", "Bachelor's Degree", "Master's Degree", "Doctorate (PhD)", "Professional Certification", "Other"]} placeholder={data.education} onValueChange={(option) => handleSelectChange('education', option)} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.education || ''}
+                                </div>
+                            </div>
+
+                            <div className="w-full max-w-64  min-w-32">
+                                <label className="text-sm text-customGray-900">Experience</label>
+                                <Select options={["No experience", "Less than 1 year", "1–2 years", "2–5 years", "5–7 years", "7–10 years", "10+ years"]} placeholder={data.experience} onValueChange={(option) => handleSelectChange('experience', option)} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.experience || ''}
+                                </div>
+                            </div>
+
+
+                            <div className="w-full max-w-64  min-w-32  ">
+                                <label className="text-sm text-customGray-900">Job Level</label>
+                                <Select options={["Entry Level", "Junior", "Mid Level", "Senior", "Lead", "Manager", "Director", "Executive"]} placeholder={data.jobLevel} onValueChange={(option) => handleSelectChange('jobLevel', option)} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.jobLevel || ''}
+                                </div>
+                            </div>
+
+
+                            <div className="w-full max-w-64  min-w-32  ">
+                                <label className="text-sm text-customGray-900">Job Type</label>
+                                <Select options={["Full-Time", "Part-Time", "Freelance", "Internship", "Temporary"]} placeholder={data.jobType} onValueChange={(option) => handleSelectChange('jobType', option)} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.jobType || ''}
+                                </div>
+                            </div>
+
+
+                            <div className="w-full max-w-64  min-w-32  ">
+                                <label className="text-sm text-customGray-900">Work Mode</label>
+                                <Select options={["Remote", "On-site", "Hybrid"]} placeholder={data.workMode} onValueChange={(option) => handleSelectChange('workMode', option)} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.workMode || ''}
+                                </div>
+                            </div>
+
+                            {['On-site', 'Hybrid'].includes(data.workMode) &&
+
+                                <div className="flex  flex-col max-w-64 min-w-32 ">
+                                    <label htmlFor="city" className="text-sm text-customGray-900">City</label>
+                                    <input type="text" placeholder="e.g. Kabul" id="city" value={data.city} onChange={(e) => setData('city', e.target.value)} className="mt-2 rounded-md border border-customGray-100 placeholder:text-customGray-400 text-customGray-900 outline-none focus:ring-1 focus:ring-primary-500 py-[11px] px-[18px]" />
+                                    <div className="text-sm w-full text-danger-600 min-h-5" >
+                                        {errors.city}
+                                    </div>
+                                </div>
+
+                            }
+
+
+
+                            <div className="w-full max-w-64  min-w-32">
+                                <label className="text-sm text-customGray-900" htmlFor="dob">Application Deadline</label>
+                                <DatePicker handleChange={handleDeadlineChange} currentDate={data.deadline} type={'date'} dateRange={'future'} />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.deadline || ''}
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="">
+                            <div className="relative mt-6">
+                                <label className="font-medium text-sm text-customGray-900">Job Description</label>
+                                <RichTextEditor content={data.description} onChange={newContent => setData('description', newContent)}
+                                    placeholder="Add your job description..." />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.description || ''}
+                                </div>
+                            </div>
+
+                            <div className="relative mt-6">
+                                <label className="font-medium text-sm text-customGray-900">Responsibilities</label>
+                                <RichTextEditor content={data.responsibilities} onChange={newContent => setData('responsibilities', newContent)}
+                                    placeholder="Add your job responsibilities..." />
+                                <div className="text-sm w-full text-danger-600 min-h-5" >
+                                    {errors.responsibilities || ''}
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="flex flex-wrap items-center gap-2 mt-6">
+                        <button disabled={processing} className="text-nowrap px-8 py-4 text-white rounded-sm bg-primary-500 hover:bg-primary-600 disabled:bg-primary-100 font-semibold cursor-pointer">
+                            Save Changes
+                        </button>
+                        <span className={`text-success-500 h-6 w-52 text-sm ${successMsg ? 'opacity-100' : 'opacity-0'}  transition-all duration-300 `}>
+                            {successMsg}
+                        </span>
+                    </div>
+                </form>
+
+
+
+
+            </div>
+        </div>,
+        root
+    )
+}
--- resources/js/Components/EmployerJob.jsx
+++ resources/js/Components/EmployerJob.jsx
@@ -1,9 +1,13 @@
 
 import { useState, useRef, useEffect } from "react"
+import EditJobModal from "./EditJobModal"
 
 
 
 export default function EmployerJob({ title, type, deadline }) {
+
+
+    const [showEditJobModal, setShowEditJobModal] = useState(false)
 
     const today = new Date()
     const endDate = new Date(deadline)
@@ -22,6 +26,7 @@ export default function EmployerJob({ title, type, deadline }) {
     const [showMenu, setShowMenu] = useState(false)
     const menuRef = useRef(null)
     const threeDotsBtnRef = useRef(null)
+
 
     useEffect(() => {
         const handleClickOutside = (e) => {
@@ -83,10 +88,10 @@ export default function EmployerJob({ title, type, deadline }) {
 
 
             <td className="p-5 whitespace-nowrap">
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 ">
                     <button type="button" className="flex gap-3  font-semibold text-primary-500 hover:text-white bg-customGray-50 hover:bg-primary-500 cursor-pointer px-6 py-3 duration-150 text-nowrap">View Applications</button>
                     <div className="relative">
-                        <button type="button" ref={threeDotsBtnRef} onClick={() => setShowMenu(prev => !prev)} className=" cursor-pointer scale-110 active:scale-125  text-customGray-500" >
+                        <button type="button" ref={threeDotsBtnRef} onClick={() => setShowMenu(prev => !prev)} className="hover:bg-customGray-50 py-[10px] px-2 rounded-sm cursor-pointer scale-110 active:scale-125  text-customGray-500" >
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 13.125C12.6213 13.125 13.125 12.6213 13.125 12C13.125 11.3787 12.6213 10.875 12 10.875C11.3787 10.875 10.875 11.3787 10.875 12C10.875 12.6213 11.3787 13.125 12 13.125Z" fill="currentColor" stroke="currentColor" />
                                 <path d="M12 6.65039C12.6213 6.65039 13.125 6.14671 13.125 5.52539C13.125 4.90407 12.6213 4.40039 12 4.40039C11.3787 4.40039 10.875 4.90407 10.875 5.52539C10.875 6.14671 11.3787 6.65039 12 6.65039Z" fill="currentColor" stroke="currentColor" />
@@ -94,17 +99,27 @@ export default function EmployerJob({ title, type, deadline }) {
                             </svg>
                         </button>
 
-                        <div ref={menuRef} className={`bg-white  min-w-32 md:min-w-40 rounded-md absolute -left-32 top-11  shadow-lg z-10 ${showMenu ? 'max-h-40 py-2' : 'max-h-0 py-0'} overflow-hidden transition-all duration-75  `}>
+                        <div ref={menuRef} className={`bg-white  min-w-32 md:min-w-40  border-customGray-50 rounded-md absolute -left-32 top-11  shadow-lg z-10 ${showMenu ? 'max-h-40 py-2 border' : 'max-h-0 py-0'} overflow-hidden transition-all duration-75  `}>
+                            <button type="button" onClick={() => {setShowEditJobModal(true); setShowMenu(false)}}
+                                className="text-customGray-600 hover:text-primary-500  flex gap-1.5 px-2  w-full min-h-8 items-center hover:bg-primary-50 transition-colors duration-150 cursor-pointer">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7.5 16.8759H3.75C3.58424 16.8759 3.42527 16.81 3.30806 16.6928C3.19085 16.5756 3.125 16.4166 3.125 16.2509V12.7598C3.125 12.6777 3.14117 12.5964 3.17258 12.5206C3.20398 12.4448 3.25002 12.3759 3.30806 12.3178L12.6831 2.94282C12.8003 2.82561 12.9592 2.75977 13.125 2.75977C13.2908 2.75977 13.4497 2.82561 13.5669 2.94282L17.0581 6.43394C17.1753 6.55115 17.2411 6.71012 17.2411 6.87588C17.2411 7.04164 17.1753 7.20061 17.0581 7.31782L7.5 16.8759Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M10.625 5L15 9.375" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M16.8753 16.8743H7.50027L3.16504 12.5391" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                                <span className="text-sm font-medium">Edit Job</span>
+                            </button>
+
                             <button type="button"
-                                className="text-customGray-600 hover:text-primary-500 flex gap-1 px-2  w-full min-h-8 items-center hover:bg-primary-50 transition-colors duration-150 cursor-pointer">
+                                className="text-customGray-600 hover:text-primary-500  flex gap-1.5 px-2  w-full min-h-8 items-center hover:bg-primary-50 transition-colors duration-150 cursor-pointer">
                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10 3.54108C3.75 3.54108 1.25 10 1.25 10C1.25 10 3.75 16.4577 10 16.4577C16.25 16.4577 18.75 10 18.75 10C18.75 10 16.25 3.54108 10 3.54108Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                     <path d="M10 13.1251C11.7259 13.1251 13.125 11.726 13.125 10.0001C13.125 8.27417 11.7259 6.87506 10 6.87506C8.27411 6.87506 6.875 8.27417 6.875 10.0001C6.875 11.726 8.27411 13.1251 10 13.1251Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                 </svg>
-                                <span className="text-sm font-medium">View Details</span>
+                                <span className="text-sm font-medium">Go to Job</span>
                             </button>
 
-                            <button className=" text-customGray-600 hover:text-danger-500 flex gap-1 px-2  w-full min-h-8 items-center hover:bg-primary-50 transition-colors duration-150 cursor-pointer">
+                            <button className=" text-customGray-600 hover:text-primary-500  flex gap-1.5 px-2  w-full min-h-8 items-center hover:bg-primary-50 transition-colors duration-150 cursor-pointer">
                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" />
                                     <path d="M12.5 7.5L7.5 12.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -120,6 +135,8 @@ export default function EmployerJob({ title, type, deadline }) {
                 </div>
 
             </td>
+
+            <EditJobModal close={() => setShowEditJobModal(false)} showModal={showEditJobModal} />
 
 
         </tr>
