2e4d727b8f2c2efe667951156695635d2c69b412
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-08-23 01:57:37 +0430

    Implement candidate Saved Jobs page

--- resources/js/Pages/Candidate/Dashboard/FavoriteJobs.jsx
+++ resources/js/Pages/Candidate/Dashboard/FavoriteJobs.jsx
@@ -1,22 +1,32 @@
 
 
+import CandidateFavoriteJob from "../../../Components/CandidateFavoriteJob"
 import CandidateDashboardLayout from "../../../Layouts/CandidateDashboardLayout"
 import Layout from "../../../Layouts/Layout"
 
 
 
 function FavoriteJobs() {
+
     return (
-        <h1 className="text-3xl">Favorite Jobs</h1>
+        <div className="">
+            <h1 className="text-customGray-900 font-medium text-lg">Favorite Jobs <span className="text-customGray-400 font-normal">(17)</span></h1>
+
+            <div className="mt-2">
+                <CandidateFavoriteJob logo={'/chess_pattern.png'} title={"Yob"} type={'Full Time'} location={'kabulll'} salary={'1200/month'} deadline={'today'}/>
+                <CandidateFavoriteJob logo={'/chess_pattern.png'} title={"Yob"} type={'Full Time'} location={'kabulll'} salary={'1200/month'} deadline={'today'}/>
+                <CandidateFavoriteJob logo={'/chess_pattern.png'} title={"Yob"} type={'Full Time'} location={'kabulll'} salary={'1200/month'} deadline={'today'}/>
+            </div>
+        </div>
     )
 }
 
 
 FavoriteJobs.layout = page => (
     <Layout>
-        <CandidateDashboardLayout children={page} />  
+        <CandidateDashboardLayout children={page} />
     </Layout>
 
 )
 
-export default FavoriteJobs 
+export default FavoriteJobs
