c1cadedca4e5f26b0fe1430faa5fc2391b1ab98b
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-08-25 14:42:38 +0430

    Change no email Send Mail button from popup to text

--- resources/js/Pages/General/CandidateProfileModal.jsx
+++ resources/js/Pages/General/CandidateProfileModal.jsx
@@ -136,8 +136,9 @@ export default function CandidateProfileModal({ showModal, setShowModal, candida
                                     </svg>
                                 </button>
 
-                                <a title={`${!email && "No email address provided"}`} href={email ? `mailto:${email}` : undefined}
-                                    className={`${email ? "bg-primary-500 hover:bg-primary-600 cursor-pointer" : "bg-primary-200 cursor-default"} group flex gap-2 rounded-sm font-semibold text-white px-6 py-3 duration-150 text-nowrap`}>
+                                <div className="flex  flex-col items-center relative">
+                                    <a href={email ? `mailto:${email}` : undefined}
+                                        className={`${email ? "bg-primary-500 hover:bg-primary-600 cursor-pointer" : "bg-primary-200 cursor-default"} group flex gap-2 rounded-sm font-semibold text-white px-6 py-3 duration-150 text-nowrap`}>
                                         <svg className="text-white duration-150" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M21 5.25L12 13.5L3 5.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M3 5.25H21V18C21 18.1989 20.921 18.3897 20.7803 18.5303C20.6397 18.671 20.4489 18.75 20.25 18.75H3.75C3.55109 18.75 3.36032 18.671 3.21967 18.5303C3.07902 18.3897 3 18.1989 3 18V5.25Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -146,6 +147,10 @@ export default function CandidateProfileModal({ showModal, setShowModal, candida
                                         </svg>
                                         Send Email
                                     </a>
+                                    {!email &&
+                                        <span className="text-center absolute -bottom-9 text-xs max-w-40 mt-1 block text-customGray-700 ">No email provided by candidate</span>
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
