e5eb2ddfb26639c7943ad681e5835354d3be8131
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-10-15 14:20:42 +0430

    Add border gradient animation to get in touch form

--- resources/js/Pages/General/ContactUs.jsx
+++ resources/js/Pages/General/ContactUs.jsx
@@ -13,7 +13,6 @@ function ContactUs() {
         message: '',
     })
 
-    console.log(usePage().props)
     return (
         <div className="flex flex-col xl:flex-row py-12 sm:py-24 px-4 sm:px-12 gap-12 xl:gap-32 justify-center sm:items-center">
             <div className="max-w-[380px]">
@@ -28,7 +27,7 @@ function ContactUs() {
 
 
             {/* <div className=" border bg-white rounded-xl border-customGray-100 shadow-xl p-4 sm:p-8 xl:p-12"> */}
-            <form onSubmit={(e) => { e.preventDefault(); post('/contact/send') }} className=" sm:border bg-white rounded-xl border-customGray-100   sm:p-8 xl:p-12">
+            <form onSubmit={(e) => { e.preventDefault(); post('/contact/send') }} className="animated-gradient-card sm:border bg-white rounded-sm border-customGray-100   sm:p-8 xl:p-12">
                 <h3 className="font-medium text-xl sm:text-2xl text-customGray-900">Get in Touch</h3>
 
                 <div className="flex flex-col sm:flex-row sm:gap-4 mt-4 sm:mt-8 ">
